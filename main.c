3918909475a07e3570bbaddf6947f90970575cb2
Eduardo Andrade <eduardoslipz@live.com>
2020-04-07 17:10:11 +0100

    sniffer v1

--- main.c
+++ main.c
@@ -87,8 +87,20 @@ void senderThread(){
 
 	unsigned char *buffer = malloc(65535);
 
-	int receiver_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)); //verificar htons e sock_raw
-	int sender_socket = socket(PF_PACKET, SOCK_RAW, IPPROTO_RAW);
+	int receiver_socket; //= socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)); //verificar htons e sock_raw
+	int sender_socket; //= socket(PF_PACKET, SOCK_RAW, IPPROTO_RAW);
+
+
+	if((receiver_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))) == -1){
+		perror("socket");
+		exit(1);
+	}
+
+	if((sender_socket = socket(PF_PACKET, SOCK_RAW, IPPROTO_RAW)) == -1){
+		perror("socket");
+		exit(1);
+	}
+
 
 	memset(&source_addr, 0, sizeof(struct sockaddr_ll));
     source_addr.sll_family = AF_PACKET;
@@ -99,6 +111,52 @@ void senderThread(){
         close(receiver_socket);
     }
 
+    memset(&dest_addr, 0, sizeof(struct sockaddr_ll));
+    dest_addr.sll_family = AF_PACKET;
+    dest_addr.sll_protocol = htons(ETH_P_ALL);
+    dest_addr.sll_ifindex = if_nametoindex("eth1");
+    if (bind(sender_socket, (struct sockaddr*) &dest_addr, sizeof(dest_addr)) < 0) {
+      perror("bind failed\n");
+      close(sender_socket);
+    }
+
+    struct ifreq ifr;
+    memset(&ifr, 0, sizeof(ifr));
+    snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "wlan0");
+    if (setsockopt(sender_socket, SOL_SOCKET, SO_BINDTODEVICE, (void *)&ifr, sizeof(ifr)) < 0) {
+        perror("bind to wlan0");
+    }
+
+    while(1)
+    {
+        source_addr_size = sizeof (struct sockaddr);
+        dest_addr_size = sizeof (struct sockaddr);
+        //Receive a packet
+        data_size = recvfrom(receiver_socket , buffer , 65536 , 0 ,(struct sockaddr *) &source_addr , (socklen_t*)&source_addr_size);
+
+        if(data_size <0 )
+        {
+            printf("Recvfrom error , failed to get packets\n");
+            return 1;
+        }
+        else{
+        printf("Received %d bytes\n",data_size);
+
+        //Huge code to process the packet (optional)
+
+        //Send the same packet out
+        bytes_sent=write(sender_socket,buffer,data_size);
+        printf("Sent %d bytes\n",bytes_sent);
+         if (bytes_sent < 0) {
+            perror("sendto");
+            exit(1);
+         }
+
+        }
+    }
+
+    close(receiver_socket);
+    return 0;
 
 }
 
